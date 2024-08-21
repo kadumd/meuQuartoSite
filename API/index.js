dbd338b88d541d1797ed5dc76c451a84d47f4a7f
cadumd <cadu.md2021@gmail.com>
2024-08-21 20:35:34 -0300

    ghyghu

--- API/index.js
+++ API/index.js
@@ -7,7 +7,7 @@ const portaDeEntrada = process.env.PORT || 3000;
 const servidor = http.createServer((pedido, resposta) => {
     console.log(pedido.url)
     if (pedido.url === '/') {
-        fs.readFile('frontend/view/index.html', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/index.html', (erro, dadosDoArquivo) => {
             if (erro) {
                 console.log(erro)
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -20,7 +20,7 @@ const servidor = http.createServer((pedido, resposta) => {
         })
     }
     if (pedido.url === '/mangas') {
-        fs.readFile('frontend/view/mangas.html', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/mangas.html', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -32,7 +32,7 @@ const servidor = http.createServer((pedido, resposta) => {
         })
     }
     if (pedido.url === '/filmes') {
-        fs.readFile('frontend/view/filmes.html', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/filmes.html', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -44,7 +44,7 @@ const servidor = http.createServer((pedido, resposta) => {
         })
     }
     if (pedido.url === '/css/index.css') {
-        fs.readFile('frontend/view/css/index.css', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/css/index.css', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -56,7 +56,7 @@ const servidor = http.createServer((pedido, resposta) => {
         })
     }
     if (pedido.url === '/css/mangas.css') {
-        fs.readFile('frontend/view/css/mangas.css', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/css/mangas.css', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -68,7 +68,7 @@ const servidor = http.createServer((pedido, resposta) => {
         })
     }
     if (pedido.url === '/css/filmes.css') {
-        fs.readFile('frontend/view/css/filmes.css', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/css/filmes.css', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -81,7 +81,7 @@ const servidor = http.createServer((pedido, resposta) => {
     }
 
     if (pedido.url === '/js/mangas.js') {
-        fs.readFile('frontend/view/js/mangas.js', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'frontend/view/js/mangas.js', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
@@ -95,7 +95,7 @@ const servidor = http.createServer((pedido, resposta) => {
 
     if (pedido.url === '/database/listaDeAnimes.json') {
         console.log("oi")
-        fs.readFile('database/listaDeAnimes.json', (erro, dadosDoArquivo) => {
+        fs.readFile(__dirname + 'database/listaDeAnimes.json', (erro, dadosDoArquivo) => {
             if (erro) {
                 console.log(erro)
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
